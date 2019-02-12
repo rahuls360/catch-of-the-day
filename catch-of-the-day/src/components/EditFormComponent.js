a045dfca3cec5d52179bbbd68866547979a5d359
Rahul Makhija <rahuls360@gmail.com>
2019-02-12 16:34:45 +0530

    editformcomponent now displays content

--- catch-of-the-day/src/components/EditFormComponent.js
+++ catch-of-the-day/src/components/EditFormComponent.js
@@ -1,40 +1,49 @@
 import React from "react";
 
 class EditFormComponent extends React.Component {
+  handleChange = (event) => {
+    console.log(event.currentTarget.value);
+    console.log("DATA CHANGING!");
+  }
   render() {
     return (
       <div className="fish-edit">
         <input
           type="text"
           name="name"
-          value={this.props.name}
+          value={this.props.fish.name}
           required
           placeholder="Fish Name"
+          onChange={this.handleChange}
         />
         <input
           type="text"
           name="price"
-          value={this.props.price}
+          value={this.props.fish.price}
           required
           placeholder="Price"
+          onChange={this.handleChange}
         />
-        <select name="status" value={this.props.status} required>
+        <select name="status" value={this.props.fish.status} required
+        onChange={this.handleChange}>
           <option value="">Select Status</option>
           <option value="available">In Stock</option>
           <option value="unavailable">Sold out</option>
         </select>
         <textarea
           name="desc"
-          value={this.props.desc}
+          value={this.props.fish.desc}
           required
           placeholder="Description"
+          onChange={this.handleChange}
         />
         <input
           type="text"
           name="image"
-          value={this.props.image}
+          value={this.props.fish.image}
           required
           placeholder="Image"
+          onChange={this.handleChange}
         />
       </div>
     );
